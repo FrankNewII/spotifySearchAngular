53a90de3a88af7d4faaeac2bee817a7d4d8d5cc0
Chunikhin Artem <chunikhin.artem92@gmail.com>
2017-03-02 19:45:37 +0200

    gitignore

--- spotifySearchFactory.js
+++ spotifySearchFactory.js
@@ -3,11 +3,39 @@
 
     var url = 'https://api.spotify.com/v1/search';
 
+    /*Future interface*/
+    var spotifyServiceInterface = {};
+
+    spotifyServiceInterface.get = {};
+    spotifyServiceInterface.get.artist = {};
+    spotifyServiceInterface.get.artist.byID = {};
+    spotifyServiceInterface.get.artist.byName = {};
+    spotifyServiceInterface.get.artist.byAlbum = {};
+
+    spotifyServiceInterface.get.artists = {};
+    spotifyServiceInterface.get.artists.byName = {};
+    spotifyServiceInterface.get.artists.byAlbum = {};
+
+    spotifyServiceInterface.get.album = {};
+    spotifyServiceInterface.get.album.byName = {};
+    spotifyServiceInterface.get.album.byId = {};
+
+    spotifyServiceInterface.get.track = {};
+    spotifyServiceInterface.get.track.byName = {};
+    spotifyServiceInterface.get.track.byId = {};
+
+    spotifyServiceInterface.get.playlist = {};
+    spotifyServiceInterface.get.playlist.byName = {};
+    spotifyServiceInterface.get.playlist.byId = {};
+
+
     function reguestToSpotify(cb) {
       $http(formatURI()).success(cb);
     }
 
-    function formatURI(artist, album, song) {
+    function formatURI(searchTagsObject) {
+      var queryString = "?";
+
       return {
         query: (arguments[0] ? 'artist:' + arguments[0] : '')
         + (arguments[1] ? ' album:' + arguments[1] : '')
@@ -17,44 +45,7 @@
       };
     }
 
-    return {
-      get: {
-        artists: {
-          byName: function (options, callback) {
-
-          }
-        },
-        artist: {
-          byId: function (options, callback) {
-
-          },
-          byName: function (options, callback) {
-
-          }
-        },
-        albums: {
-          byName: function (options, callback) {
-
-          }
-        },
-        album: {
-          byId: function (options, callback) {
-
-          },
-          byName: function (options, callback) {
-
-          }
-        },
-        songs: {
-          byId: function (options, callback) {
-
-          },
-          byName: function (options, callback) {
-
-          }
-        }
-      }
-    };
+    return spotifyServiceInterface;
   }
   ]);
 })();
